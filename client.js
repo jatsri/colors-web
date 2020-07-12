ade8a12b5084b21748c9ba46ce2c9c8183a07cf1
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-07-12 20:26:54 +0200

    Set up redux project

---
+++ client.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {hydrate} from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+
+const state = window.__STATE__;
+
+delete window.__STATE__;
+
+const store = createStore(combineReducers({}), state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+hydrate(
+    <Provider store={store}>
+        <div>Hello World</div>
+    </Provider>,
+    document.getElementById('app')
+);
---
+++ server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import React from 'react';
+import path from 'path';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { renderToString } from 'react-dom/server';
+
+const app = express();
+
+app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
+
+app.listen('5000', () => {
+    console.log('Server is listening on 5000');
+})
+
+const initialState = {};
+const store = createStore(combineReducers({}), initialState);
+
+const html = renderToString(
+    <Provider store={store}>
+        <div>Hello World</div>
+    </Provider>
+)
+
+app.get('/', (req, res) => {
+    const preloadedState = store.getState();
+    const scripts = ` <script>
+                   window.__STATE__ = ${JSON.stringify(preloadedState)}
+                </script>
+                <script src="/assets/client.js"></script>
+                `;
+
+    const html = `<!DOCTYPE html>
+              <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <title> react-app </title>
+              </head>
+              <body>
+                <div class="content">
+                   <div id="app" class="wrap-inner">
+                      ${html}
+                   </div>
+                </div>
+                ${scripts}
+              </body>`
+
+    res.send(html);
+});
