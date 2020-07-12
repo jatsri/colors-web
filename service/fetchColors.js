c6d18f23ab3128262d574ec02099a564df5ed238
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-07-13 00:55:29 +0200

    Create fetch colors action

---
+++ service/fetchColors.js
@@ -0,0 +1,13 @@
+import getFetch from '../lib/getFetch';
+import receiveColors from '../action/receiveColors';
+
+export default () => {
+    const fetch = getFetch();
+    return dispatch => {
+            return fetch('http://localhost:3000/colors')
+            .then(res => res.json())
+            .then(data => {
+                dispatch(receiveColors(data.items))
+            });
+    }
+}
